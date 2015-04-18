69a621852c91e4bcf84dccaf32cbf1299bbfa54b
Dahua Lin <lindahua@gmail.com>
2015-04-18 11:46:37 +0800

    import all sources

--- src/ConjugatePriors.jl
+++ src/ConjugatePriors.jl
@@ -1,5 +1,36 @@
 module ConjugatePriors
 
-# package code goes here
+using Compat
+using PDMats
+using Distributions
+
+import Base.LinAlg: Cholesky
+import Distributions:
+    IncompleteFormulation,
+    BinomData,
+    CategoricalData,
+
+    BernoulliStats,
+    BinomialStats,
+    CategoricalStats,
+    MultinomialStats,
+    ExponentialStats,
+    NormalStats,
+    NormalKnownSigmaStats,
+    NormalKnownMuStats,
+    MvNormalStats,
+    MvNormalKnownCovStats
+
+include("fallbacks.jl")
+include("beta_binom.jl")
+include("dirichlet_multi.jl")
+include("gamma_exp.jl")
+
+include("normalgamma.jl")
+include("normalinversegamma.jl")
+include("normalwishart.jl")
+include("normalinversewishart.jl")
+include("normal.jl")
+include("mvnormal.jl")
 
 end # module
